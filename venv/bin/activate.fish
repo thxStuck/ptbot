efa590311df775a242baca91e9988823a7bc35dc
thxStuck <haun2k15@gmail.com>
2024-10-14 22:33:16 +0300

    Initial commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ptstart/Desktop/ptbot/venv"
+set -gx VIRTUAL_ENV "/home/ubuntu/Desktop/ptbot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
